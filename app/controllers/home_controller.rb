fb0dc09eed3c27c67c58b17738e57d9619796978
Jodi Detch <jodidetch@gmail.com>
2014-07-09 12:33:53 -0400

    User must now be logged anywhere except for home/about page. App redirects user to home/index page after logged in.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,15 @@
 class HomeController < ApplicationController
 
-  def show
+  before_action :authenticate_user!, except: :about
+  # skip_before_filter :authenticate_user!, except: :about
+  # skip_before_filter :require_no_authentication, :except => [:about]
+
+  # This will be for when no user is logged in
+  def about
+  end
+
+  # This will show the default page after a user is logged in.
+  def index
   end
 
 end
