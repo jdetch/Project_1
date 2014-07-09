c355b3207e1138381338adf767d1a2bc3c9de93f
Jodi Detch <jodidetch@gmail.com>
2014-07-09 12:12:58 -0400

    App has seed data for user and 6 skin types

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+SkinType.delete_all
+
+SkinType.create!(id: 1, skin_type_number: 'I-Light', max_unprotected_exposure_time: 67)
+SkinType.create!(id: 2, skin_type_number: 'II-Fair', max_unprotected_exposure_time: 100)
+SkinType.create!(id: 3, skin_type_number: 'III-Medium', max_unprotected_exposure_time: 200)
+SkinType.create!(id: 4, skin_type_number: 'IV-Olive', max_unprotected_exposure_time: 300)
+SkinType.create!(id: 5, skin_type_number: 'V-Brown', max_unprotected_exposure_time: 400)
+SkinType.create!(id: 6, skin_type_number: 'VI-Black', max_unprotected_exposure_time: 500)
+
+
+User.create!(email: 'joe@example.com', password: '12345678', password_confirmation: '12345678')
